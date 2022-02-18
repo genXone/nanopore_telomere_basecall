a67067d7a343a565b794075ba60a37511274b47d
Maciek Sykulski <macieksk@gmail.com>
2022-02-18 20:58:16 +0100

    fixed output file ext, gx1_runme.sh

--- 1_apply_model/3_basecall_problematic_reads/gx1_runme.sh
+++ 1_apply_model/3_basecall_problematic_reads/gx1_runme.sh
@@ -8,9 +8,9 @@ mkdir -p "$RDIR/fast5_all" ; ln -f "$RDIR/fast5_"{{pass,fail}/*,all/} ;
 echo pzstd -d "$RDIR"/fast5_all/*.zst ; 
 mkdir -p "$RDIR/fast5_extracted" ; 
 mkdir -p "${SAMP}_telobasecalled" ; 
-rmdir "${SAMP}_telobasecalled" || true
+#rmdir "${SAMP}_telobasecalled" || true
 
 < ../2_identify_problematic_reads/"${SAMP}.repeatcounts.filtered.readname" mawk '{print $1}' > "${SAMP}.repeatcounts.filtered.readname" ; 
 
-perl main.pl "$RDIR/fast5_all" "${SAMP}.repeatcounts.filtered.readname"  "$RDIR/fast5_extracted" "${SAMP}_telobasecalled"
+perl main.pl "$RDIR/fast5_all" "${SAMP}.repeatcounts.filtered.readname"  "$RDIR/fast5_extracted" "${SAMP}_telobasecalled.fasta.gz"
 
